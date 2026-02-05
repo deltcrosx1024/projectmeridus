14d0215cb745c30b5f6eaeae8e8e1972d4af1a80
Boripat Kunla <167314769+deltcrosx1024@users.noreply.github.com>
2026-02-05 23:17:48 +0700

    api update for discord

--- app/api/auth/services/discord.ts
+++ app/api/auth/services/discord.ts
@@ -13,7 +13,7 @@ export async function handleDiscord(code: string, request: Request) {
   }
 
   const origin = new URL(request.url).origin;
-  const redirectUri = `${origin}/api/auth/callback?service=discord`;
+  const redirectUri = `${origin}/api/auth/callback`;
 
   const tokenResp = await fetch("https://discord.com/api/oauth2/token", {
     method: "POST",
