2c6c4d92fafa99e01d149679c13a10715f0d4369
Boripat Kunla <167314769+deltcrosx1024@users.noreply.github.com>
2026-02-20 01:22:02 +0700

    change module name

--- app/api/webhooks/services/discord/route.ts
+++ app/api/webhooks/services/discord/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 import { cookies } from 'next/headers';
 import { Octokit } from 'octokit';
 
